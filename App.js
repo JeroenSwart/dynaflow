5be0deccf7043ad410583dcc78d92723c1125a75
Thomas Booij <thomasbooij95@gmail.com>
2021-02-13 18:39:35 +0100

    Add MVP for Dynaflow

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import TaskOverview from './components/TaskOverview';
+
 export default function App() {
+  // getDynalistTask()
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>DynaFlow</Text>
+      <Text>your best friend for optimal productivity</Text>
+      <TaskOverview />
       <StatusBar style="auto" />
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#155',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
+// function getDynalistTask(){
+//     console.log('hello')
+//     axios.post('https://dynalist.io/api/v1/doc/read', 
+//        {
+//        token: 'HJFjeUECg4DbuxSRclwYPjVyNpukDcBVfUDUKJTTkvQufgv2aw23pIeiu1klGGZBAi03hKrbUbLOabj3yafVaJzF5Qncgn7fCVXtasTBgTp7wZbnzy4XsDvi0UiZhNr2',
+//        file_id: 'TLDWe8a5xrdvptlhqnkWQMUY'
+//      })
+//      .then((response) => {
+//       // console.log("succes!")
+//       console.log(response.data.title);
+
+//      }, (error) => {
+//       //  console.log(error);
+//       console.log('Failure!')
+//      });
+// } 
+
+// https://dynalist.io/api/v1/file/list
---
+++ util/ApiCalls.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import SECRETS from '../token.js'
+export default async () => {
+
+  
+    console.log('Run APIcalls script',SECRETS.TOKEN)
+    return axios.post('https://dynalist.io/api/v1/doc/read', 
+       {
+       token: SECRETS.TOKEN,
+       file_id: 'TLDWe8a5xrdvptlhqnkWQMUY'
+     })
+     .then(async response => response.data.title)
+} 
+
+
+  
+// const typeColors = {
+//     normal: 'white',
+//     fire: 'red',
+//     water: 'blue',
+//     fighting: 'white',
+//     flying: 'lavender',
+//     grass: 'green',
+//     poison: 'pink',
+//     electric: 'amber',
+//     ground: 'brown',
+//     psychic: 'purple',
+//     rock: 'darkgray',
+//     ice: 'lightblue',
+//     dragon: 'deeporange',
+//     bug: 'lightgreen',
+//     ghost: 'lightgrey',
+//     dark: 'deeppurple',
+//     fairy: 'salmon',
+//     steel: 'bluegray',
+// }
+
+// const pokemonTextColor = type => typeColors[type];
+
+// export default async name => {
+//     return axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+//       .then(response => response.data)
+//       .then(async ({id, name, sprites, types}) => ({
+//         id,
+//         name,
+//         spriteUrl: sprites.front_default,
+//         type: types[0].type.name,
+//         textColor: pokemonTextColor(types[0].type.name)
+//       }))
+//   }
